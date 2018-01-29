325bba35d006a625edca664ff245a837ff9dbbab
Damien Rottemberg <>
2018-01-29 13:31:34 -0500

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var api = new ParseServer({
   },
   push: {
 	  ios: {
-		  pfx: '/app/privateparse/prod.p12'),
+		  pfx: '/app/privateparse/prod.p12',
 		  passphrase: '$Photopon18', // optional password to your p12/PFX
 		  bundleId: 'com.photopon.Photopon',
 		  production: false
