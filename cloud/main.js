c5a4c3627917d587595e6b5f252fed4b22737f40
Damien Rottemberg <>
2018-01-30 10:56:40 -0500

    test

--- cloud/main.js
+++ cloud/main.js
@@ -265,7 +265,7 @@ Parse.Cloud.job("DailyStatSummary", function(request, status) {
 		var newMerchantsByRep = new Parse.Query("MerchantRequests");
 		newMerchantsByRep.greaterThanOrEqualTo("createdAt", start.toDate());
 		newMerchantsByRep.lessThan("createdAt", finish.toDate());
-		//newMerchantsByRep.distinct("promo");
+		newMerchantsByRep.distinct("promo");
 		//newMerchantsByRep.exist('promo');
 		
 		/*var newCoupons = new Parse.Query("Coupon");
