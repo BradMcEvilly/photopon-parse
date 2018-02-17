d316633e5f4512d85d3bb3e98a2581c243d84af7
Damien Rottemberg <>
2018-02-16 19:21:07 -0500

    reset link

--- cloud/main.js
+++ cloud/main.js
@@ -116,6 +116,51 @@ Parse.Cloud.define("getUserSessionToken", function(request, response) {
 });
 
 
+Parse.Cloud.define("resetPhotoponUserClient", function(request, response) {
+
+    //Parse.Cloud.useMasterKey();
+
+   var file = fs.readFileSync("/app/template/password_reset_email.html", "utf8");
+   var template = _.template(file);
+	   
+	   
+
+    var email = request.params.email;
+    
+
+    var query = new Parse.Query(Parse.User);
+    query.equalTo("email", email);
+    query.limit(1);
+
+    query.first({useMasterKey: true}).then(function(user) {
+	   var password  = Math.random().toString(36).slice(-8);
+	  
+	   user.set("password",password);
+	   user.save(null, {
+					useMasterKey: true,
+					success: function(user) {
+						
+					},
+					error: function(user, error) {
+						
+					}
+				});
+			
+	   
+	   var mailOptions = {
+				from: '"Photopon" <noreply@photopon.com>', 
+				subject: 'Reset Password Link', 
+				html: template({name:user.get("username"),password: password })
+		};
+		mailOptions.to ="david@ezrdv.org"; //users[i].get('email')
+	   transporter.sendMail(mailOptions, (error, info) => {});
+							
+	}, function(error) {
+        response.error("User does't exist");
+	});	   
+	   		
+
+});
 
 
 Parse.Cloud.define("GetMerchantPhotopons", function(request, response) {
