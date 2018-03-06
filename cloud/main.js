2f627d009b0478c72e30ff24a129f8c7c0c5a995
Damien Rottemberg <>
2018-03-06 11:29:43 -0500

    EIN merchant

--- cloud/main.js
+++ cloud/main.js
@@ -500,13 +500,55 @@ Parse.Cloud.define("ServerTime", function(request, response) {
 
 
 Parse.Cloud.beforeSave("MerchantRequests", function(request, response) {
+		
+		if(!request.object.existed()){
+		
+		
+			var promises = [];
+			
+			var MerchantRequest = Parse.Object.extend("MerchantRequest");
+			var queryMerchantRequest = new Parse.Query(MerchantRequest);
+			queryMerchantRequest.equalTo("taxID",request.object.get("numTried"));
+			
+			var Company = Parse.Object.extend("Company");
+			var queryCompany = new Parse.Query(Company);
+			queryCompany.equalTo("taxID",request.object.get("numTried"));
+			
+			
+			promises.push(queryMerchantRequest.first({useMasterKey: true}));
+			promises.push(queryCompany.first({useMasterKey: true}));
+		
+			Parse.Promise.when(promises).then(function(result) {
+			
+				if(result[0] ||  result[1]){
+				
+					response.error("A company with this EIN already exists");
 
+				
+				}else{
+				
+					response.success();
+				}
+			
+			
+			}).catch(function(error) {
+			
+			
+					response.error(error);
+
+			
+			});
+		
+		
+		}else{
+		
+			response.success();
+		}
 
 		
 		
 		
-		response.error("A company with this EIN already exists");
-
+		
 
 });
 
