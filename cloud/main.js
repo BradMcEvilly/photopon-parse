4a195e8d29358ef095a1ade5e2fee7c5bacb5c62
Damien Rottemberg <drottemberg@yahoo.fr>
2018-07-28 19:23:19 -0400

    merchants list

--- cloud/main.js
+++ cloud/main.js
@@ -131,6 +131,23 @@ Parse.Cloud.define("getMerchantRequests", function(request, response) {
 });
 
 
+Parse.Cloud.define("getMerchants", function(request, response) {
+
+		var query = new Parse.Query("Company");
+		query.include("merchant");
+		
+			query.find({useMasterKey: true}).then(function(results) {
+			
+				response.success(results);			
+			}).catch(function(error) {
+				response.error(new Error('Failed to get merchant'));
+			});
+
+
+});
+
+
+
 Parse.Cloud.define("resetPhotoponUserClient", function(request, response) {
 
     //Parse.Cloud.useMasterKey();
