38670a6f84e9e948a74a198862848225f005554b
stephan.com <stephan@stephan.com>
2018-08-15 01:09:39 -0400

    [#156280597] prettify beforeSave MerchantRequests

--- cloud/main.js
+++ cloud/main.js
@@ -557,57 +557,33 @@ Parse.Cloud.define("ServerTime", function(request, response) {
 
 
 Parse.Cloud.beforeSave("MerchantRequests", function(request, response) {
-		
-		if(!request.object.existed()){
-		
-		
-			var promises = [];
-			
-			var MerchantRequest = Parse.Object.extend("MerchantRequests");
-			var queryMerchantRequest = new Parse.Query(MerchantRequest);
-			queryMerchantRequest.equalTo("taxID",request.object.get("taxID"));
-			
-			var Company = Parse.Object.extend("Company");
-			var queryCompany = new Parse.Query(Company);
-			queryCompany.equalTo("taxID",request.object.get("taxID"));
-			
-			request.log.info(request.object.get("taxID"));
-			
-			promises.push(queryMerchantRequest.first({useMasterKey: true}));
-			promises.push(queryCompany.first({useMasterKey: true}));
-		
-			Parse.Promise.when(promises).then(function(result) {
-				request.log.info(pretty(result));
-				if(result[0] ||  result[1]){
-				
-					response.error("A company with this EIN already exists");
+  if(request.object.existed()) { return response.success(); }
 
-				
-				}else{
-				
-					response.success();
-				}
-			
-			
-			}).catch(function(error) {
-			
-			
-					response.error(error);
+  var promises = [];
 
-			
-			});
-		
-		
-		}else{
-		
-			response.success();
-		}
+  var MerchantRequest = Parse.Object.extend("MerchantRequests");
+  var queryMerchantRequest = new Parse.Query(MerchantRequest);
+  queryMerchantRequest.equalTo("taxID",request.object.get("taxID"));
 
-		
-		
-		
-		
+  var Company = Parse.Object.extend("Company");
+  var queryCompany = new Parse.Query(Company);
+  queryCompany.equalTo("taxID",request.object.get("taxID"));
 
+  request.log.info(request.object.get("taxID"));
+  
+  promises.push(queryMerchantRequest.first({useMasterKey: true}));
+  promises.push(queryCompany.first({useMasterKey: true}));
+
+  Parse.Promise.when(promises).then(function(result) {
+    request.log.info(pretty(result));
+    if(result[0] || result[1]) {
+      response.error("A company with this EIN already exists");
+    }else{
+      response.success();
+    }
+  }).catch(function(error) {
+    response.error(error);
+  });
 });
 
 
